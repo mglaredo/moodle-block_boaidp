413c9769b873c45ba2d7df1aec6f6ff36bd0e29e
Miguel Gonzalez Laredo <mglaredo@ugr.es>
2012-04-03 13:43:18 +0200

    README without extension on root directory and moodle version requires added in version.php (2)

--- version.php
+++ version.php
@@ -11,12 +11,12 @@
  
 defined('MOODLE_INTERNAL') || die();
  
-$plugin->version   = 2012031900;
+$plugin->version   = 2012031901;
 $plugin->requires  = 2007101550; //minimum 1.9.5  // See http://docs.moodle.org/dev/Moodle_Versions
 $plugin->cron      = 0;
 $plugin->component = 'block_boaidp';
 $plugin->maturity  = MATURITY_RC; //BETA; //MATURITY_STABLE;
-$plugin->release   = '1.3.1';
+$plugin->release   = '1.3.2';
  
 $plugin->dependencies = array(
     'mod_forum' => ANY_VERSION,
