b9dc2c215b28ce749e9431966572a7cdf52c8f55
mglaredo <mglaredo@ugr.es>
2013-07-17 13:34:30 +0200

    BETA Release for Moodle 2.x

---
+++ lang/en/block_boaidp.php
@@ -0,0 +1,59 @@
+<?php
+
+//From NEWBLOCK 
+$string['blockstring'] = 'Block string';
+$string['descconfig'] = 'Description of the config section';
+$string['descfoo'] = 'Config description';
+$string['headerconfig'] = 'Config section header';
+$string['labelfoo'] = 'Config label';
+$string['boaidp:addinstance'] = 'Add a newblock block';
+$string['boaidp:myaddinstance'] = 'Add a newblock block to my moodle';
+//$string['pluginname'] = 'Newblock';
+
+//BLOCK'S GENERAL STRINGS
+$string['pluginname'] = 'OAI METADATA RECORD';
+$string["oaidp_report"] = 'What my report is called';
+$string["boaidp"] = 'OAI METADATA RECORD';
+$string["configcontent"] = 'Texto Asociado';
+$string["last_operations"] = '<br> <u>Last Operations:</u> <br>';
+$string["not_setted"]=        '(Not setted)';
+$string["block_boaidp_fieldName"]=        'Field Name';
+$string["block_boaidp_confirmNewField"]=        'You have decided to create the following metadata type: ';
+$string["defaultvalue"]=        '(Empty)';
+
+//OPERATION MESSAGES
+$string["empty_record"] =   ' - Empty record <br>';
+$string["config_set"]=      ' - Config set <br>';
+$string["config_not_set"]=  ' - Config not set <br>';
+$string["reset_id"]=        ' - Reset id <br>';
+
+
+
+//OAI RECORDS COLUMNS/FIELDS
+$string["id"] = "id";
+$string["url"] = "url";
+$string["provider"] = "provider";
+$string["enterdate"] = "enterdate";
+$string["oai_identifier"] = "oai_identifier";
+$string["oai_set"] = "oai_set";
+$string["datestamp"] = "datestamp";
+$string["deleted"] = "deleted";
+$string["dc_title"] = "dc_title";
+$string["dc_creator"] = "dc_creator";
+$string["dc_subject"] = "dc_subject";
+$string["dc_description"] = "dc_description";
+$string["dc_contributor"] = "dc_contributor";
+$string["dc_publisher"] = "dc_publisher";
+$string["dc_date"] = "dc_date";
+$string["dc_type"] = "dc_type";
+$string["dc_format"] = "dc_format";
+$string["dc_identifier"] = "dc_identifier";
+$string["dc_source"] = "dc_source";
+$string["dc_language"] = "dc_language";
+$string["dc_relation"] = "dc_relation";
+$string["dc_coverage"] = "dc_coverage";
+$string["dc_rights"] = "dc_rights";
+
+// OTHER FORM STRINGS
+$string["new_multiple_value"] = "Add new multiple value to";
+?>
